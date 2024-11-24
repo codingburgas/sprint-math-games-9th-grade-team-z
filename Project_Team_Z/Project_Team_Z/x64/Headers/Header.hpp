3af714a851e14db3b4f4dcc5861ee9d8bd7cf128
ADKokalarov23 <ADKokalarov23@codingburgas.bg>
2024-11-24 23:10:41 +0200

    Added Headers file and moved functions

---
+++ Project_Team_Z/Project_Team_Z/x64/Headers/Header.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std;
+
+string message = "You have 5 tries to get the word";
+void hang_man(char);
+int checkGuess(char, string, string&);
+//Functions for showing ASCII for menu
